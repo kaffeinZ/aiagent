4af885d3efd5bd3111813a0fe4fc7bd6f344d2f9
kaffeinZ <kaffeinz@hotmail.com>
2025-12-21 17:24:43 +0000

    Remove build artifacts from tracking

--- plugin-trader/dist/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { starterPlugin } from './plugin.ts';
-export { starterPlugin, StarterService } from './plugin.ts';
-export { WalletService } from './wallet-service.ts';
-export { TradingService } from './trading-service.ts';
-export default starterPlugin;
-//# sourceMappingURL=index.d.ts.map
